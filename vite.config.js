19998da6cc6e9b32cbaebaec36781126da88bf91
yedu@7 <yedu7668@gmail.com>
2024-07-03 01:29:27 +0530

    update package.json file

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'my-custom-dist', // Specify your custom output directory
+  },
+});
