60e5b4cbba3ea346b404f7386b1f4972df8c309b
yedu@7 <yedu7668@gmail.com>
2024-07-03 02:02:45 +0530

    update package.json file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Provider from "./lib/context/MDProvider";
 import Layout from "./components/Layout";
 import './globals.css';
 
-const App = () =>  <Provider><Layout /></Provider>
+const App = () => {
+    return <Provider><Layout /></Provider>
+}
 
 export default App
